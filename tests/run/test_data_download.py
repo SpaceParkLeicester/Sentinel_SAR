d930e806c47b45291fb613fa4155cdeece94b027
vrym2 <vrym2@student.le.ac.uk>
2023-03-14 18:22:02 +0000

    recommit

--- tests/run/test_data_download.py
+++ tests/run/test_data_download.py
@@ -5,4 +5,8 @@ def test_download_s1_data():
     """Testing ASF download data"""
     # Source - https://github.com/asfadmin/Discovery-asf_search
     path_to_terminals = "Oil-Storage-Tanks/tests/data/uk_oil_terminals.xlsx"
-    data = DownloadS1Data(path_to_terminals = path_to_terminals)
+    path_to_cred_file = "Oil-Storage-Tanks/earthdata_cred.json"
+    data = DownloadS1Data(
+        path_to_terminals = path_to_terminals,
+        path_to_cred_file = path_to_cred_file)
+    assert data is not None
