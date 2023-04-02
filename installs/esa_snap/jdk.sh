4b740074a11eb65221c8fedece4bebc05a21e59c
vrym2 <vardhan609@gmail.com>
2023-04-02 12:31:21 +0000

    update jdk_sh

--- installs/esa_snap/jdk.sh
+++ installs/esa_snap/jdk.sh
@@ -8,18 +8,15 @@ sudo apt-get install libc6-i386
 # Checking if the /usr/lib/jvm exists
 link=https://www.oracle.com/uk/java/technologies/javase/javase8u211-later-archive-downloads.html
 opt_jdk=/opt/jdk1.8.0_351
+cd
 if [ -d $opt_jdk ]; then
-    cd
     echo "$opt_jdk exists!"
 else
-    cd
     echo "$opt_jdk does not exist"
     echo "Download java source file from $opt_jdk"
     if [ -d "apps/tmp" ]; then
-        cd
         echo "Download the tar file in ~/apps/tmp/"
     else
-        cd
         echo "Creating the folder ~/apps/tmp"
         mkdir apps/tmp/
     fi
@@ -31,22 +28,27 @@ fi
 # Run the script after downloading the file into $HOME/apps/tmp
 
 jdk8_file=apps/tmp/jdk-8u351-linux-i586.tar.gz
-
+cd
 if [ -f $jdk8_file ]; then
-    cd
     echo "$jdk8_file file exists"
     if [ -d "$opt_jdk" ]; then
-        cd
         echo "$opt_jdk folder exists!"
     else
-        cd
         sudo tar -zxvf $jdk8_file --directory /opt/
     fi
 else
-    cd
     echo "Download the Oracle distribution from "
     echo "https://www.oracle.com/uk/java/technologies/javase/javase8u211-later-archive-downloads.html"
     echo "Direct download is not possible as you need to accept the license agreements"
     echo "So, download manually, and move the file to /opt/"
     echo "Run this script again"
+fi
+
+jvm_usr_folder=/usr/lib/jvm
+cd
+if [ -d "$jvm_usr_folder" ]; then
+    echo "$jvm_usr_folder exists"
+else
+    sudo mkdir $jvm_usr_folder
+    sudo cp -r $opt_jdk $jvm_usr_folder
 fi
