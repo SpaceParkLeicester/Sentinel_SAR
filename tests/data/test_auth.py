9bcb6ddaa13906b2ba7abb781159acd01ccd2d8e
vrym2 <vardhan609@gmail.com>
2023-04-06 13:52:52 +0000

    try github actions test secrets chatgpt

--- tests/data/test_auth.py
+++ tests/data/test_auth.py
@@ -1,10 +1,13 @@
 """Functions to test authentication"""
+import os
 from oil_storage_tanks.data import auth_credentials
 
 def test_scihub_auth():
     """Testing scihub credentials in github workflow"""
+    username = os.environ.get('SCIHUB_USERNAME')
+    password = os.environ.get('SCIHUB_PASSWORD')
     scihub = auth_credentials()
     scihub.scihub_auth(
-        username = scihub_username,
-        password = scihub_password
+        username = username,
+        password = password
     )
