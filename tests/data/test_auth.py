bc87c2db00a78246c8538c91185d7c1897bc66a8
vrym2 <vardhan609@gmail.com>
2023-04-06 14:28:16 +0000

    try5

--- tests/data/test_auth.py
+++ tests/data/test_auth.py
@@ -1,12 +1,15 @@
 """Functions to test authentication"""
 import os
 from oil_storage_tanks.data import auth_credentials
+from oil_storage_tanks.utils import logger
 
 def test_scihub_auth():
     """Testing scihub credentials in github workflow"""
     username = os.environ.get('SCIHUB_USERNAME')
     password = os.environ.get('SCIHUB_PASSWORD')
-    scihub = auth_credentials()
+    scihub = auth_credentials(
+        log = logger()
+    )
     scihub.scihub_auth(
         username = username,
         password = password
