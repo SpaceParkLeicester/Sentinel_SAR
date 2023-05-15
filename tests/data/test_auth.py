3cc1a08303b8b1c8ab350f775fe067e36948c32f
vrym2 <vardhan609@gmail.com>
2023-05-15 19:00:27 +0000

    auth test

--- tests/data/test_auth.py
+++ tests/data/test_auth.py
@@ -1,12 +1,17 @@
 """Functions to test authentication"""
+import logging
+from logging import config
+config.fileConfig('logger.ini')
+logger = logging.getLogger(__name__)
+
 from asf_search.ASFSession import ASFSession
 from sentinelsat.sentinel import SentinelAPI
-from oil_storage_tanks.data import AuthCredentials
-from oil_storage_tanks.utils import logger
+from src.data import AuthCredentials
+
 
 def test_earthdata_auth():
     """Testing earthdata credentials"""
-    auth = AuthCredentials(log = logger())
+    auth = AuthCredentials(log = logger)
     user_session = auth.earthdata_auth()
     assert type(user_session) is ASFSession
 
@@ -14,6 +19,6 @@ def test_earthdata_auth():
 def test_scihub_auth():
     """Testing scihub credentials in github workflow"""
 
-    auth = AuthCredentials(log = logger())
+    auth = AuthCredentials(log = logger)
     api = auth.scihub_auth()
     assert type(api) is SentinelAPI
